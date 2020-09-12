95c513d9aecb4262c4251d00c73370f35d33f8bf
Michał Skrzypczyński <justflow555@gmail.com>
2020-09-12 16:00:40 +0200

    fix reset.scss path & add store modules prototype

---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navigation">
+        <div class="nav-item"></div>
+        <div class="nav-item"></div>
+        <div class="nav-item"></div>
+        <div class="nav-item"></div>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+    height: 80px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 6;
+    background: transparent;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    transition: all 0.3s;
+
+    &:hover {
+        background: rgba(255, 255, 255, 0.8);
+        cursor: pointer;
+    }
+
+    & .nav-item {
+        font-size: 22px;
+        color: white;
+    }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<template> <div></div></template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
