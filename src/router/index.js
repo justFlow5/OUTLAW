7abe9203d666d827a2f03313ed0d055ba55d5113
Michał Skrzypczyński <justflow555@gmail.com>
2020-10-17 14:39:02 +0200

    Add reusable Text Field

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
 import Home from '../views/Home.vue';
+import Contact from '../views/Contact.vue';
 import WatchDetails from '../views/WatchDetails.vue';
 import WatchCategoryGallery from '../views/WatchCategoryGallery.vue';
 
 Vue.use(VueRouter);
 
-const routes = [{
+const routes = [
+    {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: Contact,
     },
 
     {
@@ -21,49 +30,14 @@ const routes = [{
 
     {
         path: '/watches/:id',
-        // name: 'WatchDetails',
         component: WatchDetails,
         props: true,
     },
-
-    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    // {
-    //     path: '/classic-collection',
-
-    //     component: WatchCategoryGallery,
-    //     props: true,
-    // },
-
-    // {
-    //     path: '/professional-collection',
-
-    //     component: WatchCategoryGallery,
-    //     props: true,
-    // },
-
-    // {
-    //     path: '/watches/:category/:id',
-    //     // name: 'WatchDetails',
-    //     component: WatchDetails,
-    //     props: true,
-    // },
-
-    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
     {
         path: '/:category/:id',
-        // name: 'WatchDetails',
         component: WatchDetails,
         props: true,
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import( /* webpackChunkName: "about" */ '../views/About.vue'),
     },
 ];
 
---
+++ src/views/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="form-container"><InputField /></div>
+</template>
+
+<script>
+import InputField from '../components/contactPage/InputField';
+export default {
+    name: 'ContactPage',
+    components: {
+        InputField,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.form-container {
+    width: 50%;
+    margin: 20% auto;
+}
+</style>
